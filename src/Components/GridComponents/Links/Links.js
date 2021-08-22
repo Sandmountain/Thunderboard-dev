908816f6c2b895ebebaadf8efe416ee91fabbf6f
Viktor Sandberg <sandtain@gmail.com>
2021-08-22 17:02:40 +0200

    Recfator links component

--- src/Components/GridComponents/Links/Links.js
+++ src/Components/GridComponents/Links/Links.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { Card, IconButton, makeStyles, Tooltip, Typography } from '@material-ui/core';
 import { Check, Delete, Link } from '@material-ui/icons';
-import { openInNewTab } from '../../helperFunctions';
+import { openInNewTab, moveArrayItemToNewIndex } from '../../helperFunctions';
 import { updateFirestoreCollection } from '../../../Firestore/FirestoreFunctions';
 
 const useStyles = makeStyles({
@@ -24,10 +24,6 @@ const useStyles = makeStyles({
     height: '100%',
     width: '100%',
   },
-  deleteCursor: {
-    //cursor: `url(${deleteCursor}), all`,
-    //cursor: 'pointer',
-  },
   dragWrapper: {
     position: 'absolute',
     width: '100%',
@@ -49,6 +45,9 @@ const useStyles = makeStyles({
     display: 'grid',
     gridTemplateColumns: 'repeat(auto-fit, minmax(20px, 1fr))',
     width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
     gap: '5px',
     overflow: 'hidden',
     padding: 5,
@@ -58,6 +57,16 @@ const useStyles = makeStyles({
   },
   clickUrl: {
     cursor: 'pointer',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  linkContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100%',
+    maxHeight: 16,
   },
   addText: {
     textOverflow: 'ellipsis',
@@ -74,6 +83,11 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
   const classes = useStyles();
   const [draggedOutside, setDraggedOutside] = useState(false);
   const [isHover, setIsHover] = useState(false);
+  const [isSavedLink, setIsSavedLink] = useState(false);
+
+  const [currentDraggedLink, setCurrentDraggedLink] = useState(null);
+  const [newChanges, setNewChanges] = useState(false);
+
   const [urls, setUrls] = useState(links);
 
   const handleDragLeave = (e) => {
@@ -81,9 +95,22 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
     e.stopPropagation();
     setIsHover(false);
   };
+
   const handleDragOver = (e) => {
     e.preventDefault();
-    setIsHover(true);
+    if (!isSavedLink) {
+      setIsHover(true);
+    } else {
+      // Handles movement of links
+      const hoveredLink = e.target.querySelector('img').src;
+      const currLinkIndex = urls.findIndex((obj) => obj.href === currentDraggedLink);
+      const hoveredLinkIndex = urls.findIndex((obj) => obj.href === hoveredLink);
+
+      if (currLinkIndex !== hoveredLinkIndex) {
+        setUrls(moveArrayItemToNewIndex(urls, currLinkIndex, hoveredLinkIndex));
+        setNewChanges(true);
+      }
+    }
     e.stopPropagation();
   };
 
@@ -95,6 +122,13 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
         links: urls,
       },
     });
+    updateFirestoreCollection({
+      linksSettings: {
+        ...settings.linksSettings,
+        links: urls,
+      },
+    });
+    setNewChanges(false);
   };
 
   const isOutside = (targetX, targetY, container) => {
@@ -107,8 +141,11 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
     }
   };
 
-  const dragLink = (e) => {
+  const dragLink = (e, href) => {
     e.preventDefault();
+    setIsSavedLink(true);
+    setCurrentDraggedLink(href);
+
     const outside = isOutside(e.clientX, e.clientY, cardContainer);
     if (outside) {
       setDraggedOutside(true);
@@ -117,9 +154,12 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
     }
     e.stopPropagation();
   };
+
   const dragLinkEnd = (e, src) => {
     isOutside(e.clientX, e.clientY, cardContainer) && setUrls(urls.filter((link) => link.src !== src));
     setDraggedOutside(false);
+    setIsSavedLink(false);
+    setCurrentDraggedLink('');
   };
 
   const handleDrop = async (e) => {
@@ -127,13 +167,19 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
     e.stopPropagation();
     setIsHover(false);
 
+    if (isSavedLink) {
+      setIsSavedLink(false);
+      setCurrentDraggedLink('');
+      return;
+    }
+
     try {
       let url = e.dataTransfer.getData('url');
 
       if (!urls.find((link) => link.src === url)) {
         const parsedUrl = new URL(url);
         const newUrl = {
-          href: `https://i.olsh.me/icon?url=${parsedUrl.hostname}&size=12..120..200`,
+          href: `https://api.faviconkit.com/${parsedUrl.hostname}`,
           src: url,
           name: parsedUrl.hostname,
         };
@@ -184,17 +230,14 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
               {urls.map((url, idx) => {
                 return (
                   <Tooltip key={idx} placement="top" title={url.src}>
-                    <div className={classes.clickUrl}>
-                      <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
-                        <img
-                          style={{ width: '100%', objectFit: 'contain' }}
-                          onClick={() => openInNewTab(url.src)}
-                          src={url.href}
-                          alt={url.href}
-                          draggable={true}
-                          onDrag={dragLink}
-                          onDragEnd={(e) => dragLinkEnd(e, url.src)}
-                        />
+                    <div className={`${classes.clickUrl}`}>
+                      <div
+                        className={classes.linkContainer}
+                        onClick={() => openInNewTab(url.src)}
+                        draggable={true}
+                        onDrag={(e) => dragLink(e, url.href)}
+                        onDragEnd={(e) => dragLinkEnd(e, url.src)}>
+                        <img style={{ width: '100%', objectFit: 'contain' }} src={url.href} alt={url.href} />
                       </div>
                     </div>
                   </Tooltip>
@@ -211,14 +254,15 @@ export default function Links({ links, isDraggable, settings, setSettings }) {
           )}
         </div>
       </div>
-      {links !== urls && (
-        <IconButton
-          size="small"
-          style={{ position: 'absolute', right: 0, color: 'green' }}
-          onClick={() => saveChanges()}>
-          <Check></Check>
-        </IconButton>
-      )}
+      {links !== urls ||
+        (newChanges && (
+          <IconButton
+            size="small"
+            style={{ position: 'absolute', right: 0, color: 'green' }}
+            onClick={() => saveChanges()}>
+            <Check></Check>
+          </IconButton>
+        ))}
     </Card>
   );
 }
--- src/Components/helperFunctions.js
+++ src/Components/helperFunctions.js
@@ -82,3 +82,14 @@ export function cleanIframe(html) {
       allowfullscreen
     />`;
 }
+
+export function moveArrayItemToNewIndex(arr, old_index, new_index) {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+  return arr;
+}
