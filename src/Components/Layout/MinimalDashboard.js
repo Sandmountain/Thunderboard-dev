0ffa8d30f054bc4f9be2e248f9671a1342098e26
Viktor Sandberg <sandtain@gmail.com>
2022-02-27 21:37:36 +0100

    Add clock and weather to new layout

--- src/Components/Layout/MinimalDashboard.js
+++ src/Components/Layout/MinimalDashboard.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import StaticClock from '../GridComponents/TimeDate/StaticClock';
+import { makeStyles } from '@material-ui/core';
+import StaticWeatherWidget from '../GridComponents/WeatherWidget/StaticWeatherWidget';
+
+const useStyles = makeStyles({
+  mainContainer: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default function MinimalDashboard() {
-  return <div>MinimalDashboard</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mainContainer}>
+      <StaticClock />
+      <StaticWeatherWidget city={'stockholm'} />
+    </div>
+  );
 }
