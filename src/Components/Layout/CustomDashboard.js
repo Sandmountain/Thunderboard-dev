59679c93922f2a5feb4c4c07c7125c71e130985e
Viktor Sandberg <sandtain@gmail.com>
2022-02-27 21:41:13 +0100

    remove unused variables

--- src/Components/Layout/CustomDashboard.js
+++ src/Components/Layout/CustomDashboard.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from 'react';
+import React, { useContext } from 'react';
 
 import GoogleMailComponent from '../GridComponents/GoogleMail/GoogleMailComponent.js';
 import YoutubeComponent from '../GridComponents/YoutubeVideo/YoutubeComponent';
