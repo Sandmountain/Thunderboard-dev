72d60d3f3e6a6c9d2989e9019d192c1d7a8c6535
Viktor Sandberg <sandtain@gmail.com>
2022-03-02 21:28:39 +0100

    Create a news component

---
+++ src/Components/GridComponents/APIreader/ReaderList.js
@@ -0,0 +1,204 @@
+import { Box, Card, makeStyles, Popover, Typography } from '@material-ui/core';
+import { OpenInNew } from '@material-ui/icons';
+import React, { useState, useRef } from 'react';
+
+import { openInNewTab } from '../../helperFunctions';
+
+const useStyles = makeStyles({
+  articleText: {
+    paddingRight: '12px',
+    alignSelf: 'center',
+  },
+  iconContainer: {
+    marginLeft: 'auto',
+    alignSelf: 'center',
+    display: 'inline-flex',
+  },
+  articleCard: {
+    transition: 'none',
+    borderRadius: 0,
+    width: '100%',
+    padding: '5px',
+  },
+  articleCardHover: {
+    cursor: 'pointer',
+    zIndex: 2,
+    position: 'relative',
+    boxShadow:
+      '0px 2px 3px -1px rgba(0,0,0,0.2), 0px -2px 3px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12), inset 0 0 0 1px #f1f1f1',
+  },
+  articleListItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    whiteSpace: 'nowrap',
+  },
+  articleListItemText: {
+    marginLeft: '0.25em',
+    width: 'auto',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+  articleListItemTextGray: {
+    color: 'gray',
+    fontWeight: '300',
+    marginLeft: '0.25em',
+  },
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    borderRadius: 0,
+    minHeight: '110px',
+  },
+  popoverTitleWrapper: {
+    height: 215,
+    overflow: 'hidden',
+  },
+  popoverTitleContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  popoverTitleText: {},
+  popoverContentContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    overflow: 'hidden',
+  },
+  popoverContentInnerContainer: {
+    width: 'calc(50% - 2.5px)',
+    height: 'auto',
+  },
+  popoverContentImage: {
+    maxHeight: 120,
+    width: 'auto',
+    maxWidth: '100%',
+  },
+});
+
+export default function ReaderList({
+  title,
+  src,
+  url,
+  date,
+  content,
+  source,
+  anchorOriginHorizontal,
+  anchorOriginVertical,
+  margin,
+  standAlone,
+}) {
+  const classes = useStyles();
+  const childRef = useRef();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [currentWidth, setCurrentWidth] = useState(0);
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handlePopoverOpen = () => {
+    setCurrentWidth(childRef.current?.parentNode.offsetParent.clientWidth);
+
+    setAnchorEl(childRef.current.parentNode.offsetParent);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <Card
+      ref={childRef}
+      aria-haspopup="true"
+      className={` ${classes.articleCard} ${open && classes.articleCardHover} 
+        `}
+      onMouseEnter={handlePopoverOpen}
+      onMouseLeave={handlePopoverClose}
+    >
+      <Typography variant="body2" component="div">
+        <Box className={classes.articleText} onClick={() => openInNewTab(url)}>
+          <div className={classes.articleListItem}>
+            <Box>{date}</Box>
+            <Box component="a" className={classes.articleListItemText}>
+              <strong>{title}</strong>
+              <span className={classes.articleListItemTextGray}>{content}</span>
+            </Box>
+
+            {open && (
+              <div className={classes.iconContainer}>
+                <OpenInNew style={{ fontSize: '1.2em' }} />
+              </div>
+            )}
+          </div>
+        </Box>
+      </Typography>
+
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        style={{ margin: 0, padding: 0 }}
+        elevation={18}
+        open={open}
+        anchorEl={anchorEl}
+        classes={{
+          paper: classes.paper,
+        }}
+        anchorReference={standAlone ? 'anchorPosition' : 'anchorEl'}
+        anchorOrigin={{
+          vertical: anchorOriginVertical,
+          horizontal: anchorOriginHorizontal,
+        }}
+        anchorPosition={{
+          top:
+            document.getElementById('newsContainer').getBoundingClientRect()
+              .top - 222,
+          left: window.innerWidth / 2 - currentWidth / 2,
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <div
+          className={classes.popoverTitleWrapper}
+          style={{
+            width: standAlone ? currentWidth : currentWidth + 150,
+          }}
+        >
+          <div style={{ padding: 5 }}>
+            <Typography
+              variant="body1"
+              className={classes.popoverTitleContainer}
+            >
+              <Box
+                component="span"
+                className={classes.popoverTitleText}
+                fontWeight={'fontWeightBold'}
+                style={{ width: currentWidth - 10 }}
+              >
+                {title}
+              </Box>
+            </Typography>
+            <div className={classes.popoverContentContainer}>
+              <div className={classes.popoverContentInnerContainer}>
+                {src && (
+                  <img
+                    src={src}
+                    className={classes.popoverContentImage}
+                    alt={title}
+                  />
+                )}
+              </div>
+              <div className={classes.popoverContentInnerContainer}>
+                <Typography align="left" variant="caption">
+                  {content}
+                </Typography>
+              </div>
+            </div>
+            <Typography align="left" variant="overline">
+              Source: {source}
+            </Typography>
+          </div>
+        </div>
+      </Popover>
+    </Card>
+  );
+}
